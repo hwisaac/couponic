c5fbdb927636e2dad692ea91b6bf053b09a3ed4c
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-03-28 18:11:00 +0900

    저장 방지 넣기

--- complete.py
+++ complete.py
@@ -136,9 +136,9 @@ for i in range(PAGE_FROM, PAGE_TO + 1):
   btns[0].click() # 닫기버튼 클릭
 
   py.sleep(2*TIME_MULTIPLIER)
-  driver.find_element(By.CLASS_NAME, 'btns_wrap').find_element(By.CLASS_NAME, 'btns_blue').click() # 저장 클릭
-  py.sleep(1 *TIME_MULTIPLIER)
-  py.press('enter') # alert 창 닫기
+  # driver.find_element(By.CLASS_NAME, 'btns_wrap').find_element(By.CLASS_NAME, 'btns_blue').click() # 저장 클릭
+  # py.sleep(1 *TIME_MULTIPLIER)
+  # py.press('enter') # alert 창 닫기
 
 
 py.sleep(10)
