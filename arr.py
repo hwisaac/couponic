a9423e4464daba9825e0bc18601aa05d0bfd696d
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-10-08 08:21:31 +0900

    1007

---
+++ arr.py
@@ -0,0 +1,54 @@
+[
+    {
+        "subject": "",
+        "description": "수업과 번역가가 알아야할 사항에 대한 오리엔테이션",
+        "video_url": None,
+        "free": False,
+    },
+    {
+        "subject": "",
+        "description": "주어와 대명사 처리",
+        "video_url": None,
+        "free": False,
+    },
+    {"subject": "", "description": "호응", "video_url": None, "free": False},
+    {
+        "subject": "",
+        "description": "단조롭지 않고 군더더기 없는 글 쓰기",
+        "video_url": None,
+        "free": False,
+    },
+    {
+        "subject": "",
+        "description": "번역투 문장 피하기 1",
+        "video_url": None,
+        "free": False,
+    },
+    {
+        "subject": "",
+        "description": "번역투 문장 피하기 2",
+        "video_url": None,
+        "free": False,
+    },
+    {"subject": "", "description": "시제의 번역", "video_url": None, "free": False},
+    {"subject": "", "description": "리듬을 살린 글", "video_url": None, "free": False},
+    {
+        "subject": "",
+        "description": "글의 종류에 따른 표현 방법",
+        "video_url": None,
+        "free": False,
+    },
+    {"subject": "", "description": "제목 정하기", "video_url": None, "free": False},
+    {
+        "subject": "",
+        "description": "의미를 확실하게 전달하는 글",
+        "video_url": None,
+        "free": False,
+    },
+    {
+        "subject": "",
+        "description": "혼동하기 쉬운 단어",
+        "video_url": None,
+        "free": False,
+    },
+]
